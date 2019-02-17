24407168a4981d6af4359257721303be50227483
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-02-16 21:05:03 -0500

    Found a more elegant way to conditionally render the CharCard (using an inline && method from the React Docs). Used a React.Fragment.

--- starwars/src/components/CharCard.js
+++ starwars/src/components/CharCard.js
@@ -2,24 +2,35 @@ import React from "react";
 
 const CharCard = props => {
   // Stops cards from displaying until a character is chosen:
-  if (!props.moreCharInfo.name) {
-    return <div className="blank-char-card" />;
-  } else {
-    return (
-      <div className="char-card">
-        <div className="card-background" />
-        <div className="card-foreground">
-          <h2>{props.moreCharInfo.name}</h2>
-          <p>Birth Year: {props.moreCharInfo.birth_year}</p>
-          <p>Gender: {props.moreCharInfo.gender}</p>
-          <p>Eye Color: {props.moreCharInfo.eye_color}</p>
-          <p>Hair Color: {props.moreCharInfo.hair_color}</p>
-          <p>Mass: {props.moreCharInfo.mass}</p>
-          <p>Height: {props.moreCharInfo.height}</p>
+  return (
+    <React.Fragment>
+      {!props.moreCharInfo.name && (
+        <div className="char-card">
+          <div className="card-background" />
+          <div className="card-foreground">
+            <h3>
+              Click a character's More Info button to find out...more
+              information about that character.
+            </h3>
+          </div>
         </div>
-      </div>
-    );
-  }
+      )}
+      {props.moreCharInfo.name && (
+        <div className="char-card">
+          <div className="card-background" />
+          <div className="card-foreground">
+            <h2>{props.moreCharInfo.name}</h2>
+            <p>Birth Year: {props.moreCharInfo.birth_year}</p>
+            <p>Gender: {props.moreCharInfo.gender}</p>
+            <p>Eye Color: {props.moreCharInfo.eye_color}</p>
+            <p>Hair Color: {props.moreCharInfo.hair_color}</p>
+            <p>Mass: {props.moreCharInfo.mass}</p>
+            <p>Height: {props.moreCharInfo.height}</p>
+          </div>
+        </div>
+      )}
+    </React.Fragment>
+  );
 };
 
 export default CharCard;
