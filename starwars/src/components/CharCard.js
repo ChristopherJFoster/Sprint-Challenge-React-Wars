9385bc5b85de7b9827617493f45e76977e5a987b
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-02-16 21:13:44 -0500

    Refactored CharCard.js to be more DRY.

--- starwars/src/components/CharCard.js
+++ starwars/src/components/CharCard.js
@@ -3,22 +3,17 @@ import React from "react";
 const CharCard = props => {
   // Stops cards from displaying until a character is chosen:
   return (
-    <React.Fragment>
-      {!props.moreCharInfo.name && (
-        <div className="char-card">
-          <div className="card-background" />
-          <div className="card-foreground">
-            <h3>
-              Click a character's More Info button to find out...more
-              information about that character.
-            </h3>
-          </div>
-        </div>
-      )}
-      {props.moreCharInfo.name && (
-        <div className="char-card">
-          <div className="card-background" />
-          <div className="card-foreground">
+    <div className="char-card">
+      <div className="card-background" />
+      <div className="card-foreground">
+        {!props.moreCharInfo.name && (
+          <h3>
+            Click a character's More Info button to find out...more information
+            about that character.
+          </h3>
+        )}
+        {props.moreCharInfo.name && (
+          <React.Fragment>
             <h2>{props.moreCharInfo.name}</h2>
             <p>Birth Year: {props.moreCharInfo.birth_year}</p>
             <p>Gender: {props.moreCharInfo.gender}</p>
@@ -26,10 +21,10 @@ const CharCard = props => {
             <p>Hair Color: {props.moreCharInfo.hair_color}</p>
             <p>Mass: {props.moreCharInfo.mass}</p>
             <p>Height: {props.moreCharInfo.height}</p>
-          </div>
-        </div>
-      )}
-    </React.Fragment>
+          </React.Fragment>
+        )}
+      </div>
+    </div>
   );
 };
 
